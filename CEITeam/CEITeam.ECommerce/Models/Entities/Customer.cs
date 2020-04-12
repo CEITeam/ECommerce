437d0016927724ce224b4d43831823da792377a4
abdelrahman fathy <abdelrahmanfathe95@gmail.com>
2020-04-12 21:04:35 +0200

    Updating Customer

--- CEITeam/CEITeam.ECommerce/Models/Entities/Customer.cs
+++ CEITeam/CEITeam.ECommerce/Models/Entities/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,5 +19,14 @@ namespace CEITeam.ECommerce.Models
         [MaxLength(100)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        //#region Navigation Property
+        ////-------Upadated 12/4/2020 ----------
+        //[ForeignKey("ApplicationUser")]
+        //public string Fk_ApplicationUserId { get; set; }
+        //public virtual ApplicationUser ApplicationUser { get; set; }
+
+        //public virtual List<Order> Orders { get; set; }
+        //#endregion
     }
 }
