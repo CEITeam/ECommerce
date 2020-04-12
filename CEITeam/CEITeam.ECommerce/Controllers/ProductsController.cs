65be627e9adfa20715c7fd6563a875e918075dd2
aGaabdelgawad <aga.abdelgawad@gmail.com>
2020-04-12 23:11:36 +0200

    Add Products Controller

---
+++ CEITeam/CEITeam.ECommerce/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CEITeam.ECommerce.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CEITeam.ECommerce.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public ProductsController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult AddEdit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
