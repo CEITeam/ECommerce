1d8db5dc71ef26cc70c4212abc254cc9295232ed
abdelrahman fathy <abdelrahmanfathe95@gmail.com>
2020-04-13 12:10:40 +0200

    JavaScript Code To Toggle Between Registeration Forms

--- CEITeam/CEITeam.ECommerce/wwwroot/js/site.js
+++ CEITeam/CEITeam.ECommerce/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(function (global, doc) {
+    let registerCustomer = () => {
+        $("#registerBrand").addClass("d-none");
+        $("#registerCustomer").removeClass("d-none");
+
+    };
+
+    let registerBrand = () => {
+        $("#registerCustomer").addClass("d-none");
+        $("#registerBrand").removeClass("d-none");
+    };
+
+    global.registerCustomer = registerCustomer;
+    global.registerBrand = registerBrand;
+}(this, document));
