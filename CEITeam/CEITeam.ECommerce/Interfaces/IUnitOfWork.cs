b3235c8f18a28eee0b57ff0d9c5e6d4454cb28d1
abdelrahman fathy <abdelrahmanfathe95@gmail.com>
2020-04-13 12:32:44 +0200

    Adding managers as properties in unit of work

--- CEITeam/CEITeam.ECommerce/Interfaces/IUnitOfWork.cs
+++ CEITeam/CEITeam.ECommerce/Interfaces/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CEITeam.ECommerce.Managers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,14 @@ namespace CEITeam.ECommerce.Interfaces
 {
     public interface IUnitOfWork
     {
+        ApplicationUserManager ApplicationUserManager { get; }
+        ProductManager ProductManager { get; }
+        CategoryManager CategoryManager { get; }
+        TagManager TagManager { get; }
+        BrandManager BrandManager { get; }
+        CustomerManager CustomerManager { get; }
+        OrderManger OrderManger { get; }
+        ProductTagManager ProductTagManager { get; }
+
     }
 }
--- CEITeam/CEITeam.ECommerce/Managers/UnitOfWork.cs
+++ CEITeam/CEITeam.ECommerce/Managers/UnitOfWork.cs
@@ -11,9 +11,88 @@ namespace CEITeam.ECommerce.Managers
     {
         private readonly ApplicationDbContext context;
 
+        private ApplicationUserManager applicationUserManager;
+        private CustomerManager customerManager;
+        private BrandManager brandManager;
+        private ProductManager productManager;
+        private TagManager tagManager;
+        private CategoryManager categoryManager;
+        private OrderManger orderManger;
+        private ProductTagManager productTagManager;
+
         public UnitOfWork(ApplicationDbContext context)
         {
             this.context = context;
+
         }
+
+        public ApplicationUserManager ApplicationUserManager
+        {
+            get
+            {
+                applicationUserManager = applicationUserManager ?? new ApplicationUserManager(context);
+                return applicationUserManager;
+            }
+        }
+        public ProductManager ProductManager
+        {
+            get
+            {
+                productManager = productManager ?? new ProductManager(context);
+                return productManager;
+            }
+        }
+        public CategoryManager CategoryManager
+        {
+            get
+            {
+                categoryManager = categoryManager ?? new CategoryManager(context);
+                return categoryManager;
+            }
+        }
+        public TagManager TagManager
+        {
+            get
+            {
+                tagManager = tagManager ?? new TagManager(context);
+                return tagManager;
+            }
+        }
+        public BrandManager BrandManager
+        {
+            get
+            {
+                brandManager = brandManager ?? new BrandManager(context);
+                return brandManager;
+            }
+        }
+        public CustomerManager CustomerManager
+        {
+            get
+            {
+                customerManager = customerManager ?? new CustomerManager(context);
+                return customerManager;
+            }
+        }
+
+        public OrderManger OrderManger
+        {
+            get
+            {
+                orderManger = orderManger ?? new OrderManger(context);
+                return orderManger;
+            }
+        }
+
+        public ProductTagManager ProductTagManager
+        {
+            get
+            {
+                productTagManager = productTagManager ?? new ProductTagManager(context);
+                return productTagManager;
+            }
+        }
+
+
     }
 }
