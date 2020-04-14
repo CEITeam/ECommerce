cdd8f0130d3ee486b6a62d721f8cd1b8aac9bde7
abdelrahman fathy <abdelrahmanfathe95@gmail.com>
2020-04-14 21:18:58 +0200

    update navbar in _layout & _LoginPartial & ProfileNavBar\default

---
+++ CEITeam/CEITeam.ECommerce/ViewComponents/ProfileNavBarViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using CEITeam.ECommerce.Interfaces;
+using CEITeam.ECommerce.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CEITeam.ECommerce.ViewComponents
+{
+    public class ProfileNavBarViewComponent:ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProfileNavBarViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string userName)
+        {
+            ApplicationUser user =userName !=null?_unitOfWork.ApplicationUserManager.GetAll().FirstOrDefault(a => a.UserName == userName):null;
+            return  View(user);
+        }
+    }
+}
